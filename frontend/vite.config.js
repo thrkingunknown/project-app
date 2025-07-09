34deeb98de33e308eef850d1701bfa5e427caa6e
thrkingunknown <131584252+thrkingunknown@users.noreply.github.com>
2025-07-09 17:04:32 +0530

    sitemap?

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import Sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    Sitemap({ hostname: "https://project-app-omega-two.vercel.app/" }),
+  ],
 });
